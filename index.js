e685e8e4416c149d9a9516353e1b1dfe7680d8fd
Divyam-Divyansh <divyamdivyansh2720@gmail.com>
2025-05-31 23:21:14 +0530

    json data

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ require('dotenv').config()
 const app = express()
 const port = 3000
 
+const userData = {
+    id: 1, title: none,
+    id:2, title: none2
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/gitData', (req, res) =>{
+    res.json({use})
 })
 
 app.listen(process.env.PORT, () => {
